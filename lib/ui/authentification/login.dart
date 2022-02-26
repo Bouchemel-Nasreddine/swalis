7dcd42180385b76cad17c0e67c3c560819c8ff61
marwa7 <im_djebli@esi.dz>
2022-02-26 16:58:25 +0100

    LoginUI

--- lib/ui/authentification/login.dart
+++ lib/ui/authentification/login.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:swalis/ui/authentification/signin_artisan.dart';
+import 'package:swalis/ui/home/home.dart';
 import '../home/home.dart';
 
 class Login extends StatefulWidget {
@@ -9,21 +12,233 @@ class Login extends StatefulWidget {
   _LoginState createState() => _LoginState();
 }
 
+Widget buildEmail(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        'Email',
+        style: TextStyle(
+          color:  Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10.0,
+      ),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Email',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildPassword(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        'Password',
+        style: TextStyle(
+          color:  Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10.0,
+      ),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          obscureText: true,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Password',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildForgotPssBtn(){
+  return Container(
+    alignment: Alignment.centerRight,
+    child: FlatButton(
+      onPressed: () => print('password forgot'),
+      padding: EdgeInsets.only(right: 0.0),
+      child: Text(
+        'Forget password ?',
+        style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+
+    ),
+  );
+}
+
+
+
+
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text('login'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Home(),
-              ),
-            );
-          },
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value:  SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration:  BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0x66EC0677),
+                          Color(0x99EC0677),
+                          Color(0xccEC0677),
+                          Color(0xffEC0677),
+                        ]
+                    )
+                ),
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 120,
+                  ),
+                  child: Column(
+
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children:  [
+                      Text(
+                        'Sign in',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      buildEmail(),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      buildPassword(),
+                      buildForgotPssBtn(),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 25.0),
+                    width: double.infinity,
+                    child: RaisedButton(
+                      elevation: 5,
+                      padding: EdgeInsets.all(15.0),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder:
+                        (context) => Home()
+                        ));
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      color:  Colors.white,
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          color:  Color(0xffEC0677),
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: FlatButton(
+                          onPressed: () => {
+                          Navigator.push(context, MaterialPageRoute(builder:
+                          (context) => SignInArtisan()
+                          ))
+                        },
+                          padding: EdgeInsets.only(right: 0.0),
+                          child: Text(
+                            'Subscribe as supplier',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+
+                        ),
+                      )
+
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/authentification/signin_artisan.dart
+++ lib/ui/authentification/signin_artisan.dart
@@ -1,5 +1,9 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../home/home.dart';
 
 class SignInArtisan extends StatefulWidget {
   const SignInArtisan({ Key? key }) : super(key: key);
@@ -8,11 +12,356 @@ class SignInArtisan extends StatefulWidget {
   _SignInArtisanState createState() => _SignInArtisanState();
 }
 
+Widget buildFirstName(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.person,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'First name',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildLastName(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.person,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Last name',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildPhoneNumber(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          keyboardType: TextInputType.number,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.phone_android,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Phone Number',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+
+Widget buildAdress(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          keyboardType: TextInputType.number,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.location_city,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Adress',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildEmail(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Email',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildPassword(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          obscureText: true,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Password',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildConfirmPassword(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0,2)
+              )
+            ]
+        ),
+        height: 60.0,
+        child: TextField(
+          obscureText: true,
+          style: TextStyle(
+              color: Colors.black87
+          ),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Color(0xffEC0677),
+              ),
+              hintText: 'Confirm password',
+              hintStyle: TextStyle(
+                  color: Colors.black87
+              )
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
 class _SignInArtisanState extends State<SignInArtisan> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('sign in artisan'),
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value:  SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration:  BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0x66EC0677),
+                          Color(0x99EC0677),
+                          Color(0xccEC0677),
+                          Color(0xffEC0677),
+                        ]
+                    )
+                ),
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 120,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:  [
+                      Text(
+                        'Sign Up supplier',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      buildFirstName(),
+                      buildLastName(),
+                      buildPhoneNumber(),
+                      buildAdress(),
+                      buildEmail(),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      buildPassword(),
+                      buildConfirmPassword()
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
